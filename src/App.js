6ed4e4c5f6e7c1d2f9a595134857650c9d50dd63
TarasMysiura <tarasmysiura@gmail.com>
2023-06-17 21:28:17 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+import { Card } from './components/Card/Card';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+function App({ special = false }) {
+  const [clicked, setClicked] = useState(false);
+
+  const className = clsx('myText', {
+    special: special,
+  });
+
+  const bColor = clicked ? 'red' : 'blue';
+
+  return (
+    <div className="App">
+      <Card fName="Alex" sName="Hnennyi" />
+      <p className={className} style={{ backgroundColor: bColor }}>
+        My Text
+      </p>
+      <button onClick={() => setClicked(!clicked)}>Click me</button>
+    </div>
+  );
+}
+
+App.propTypes = {
+  special: PropTypes.bool,
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,50 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './components/App';
+import  App  from './components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-const imgAvatarUrl = 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png';
+// const imgAvatarUrl = 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png';
 // const nameAvatar = 'Petra Marica';
-const tagAvatar = '@pmarica';
-const locationAvatar = 'Salvador, Brasil';
+// const tagAvatar = '@pmarica';
+// const locationAvatar = 'Salvador, Brasil';
 
-const Profile = props => (
-  <div className="profile">
-    <div className="description">
-      <img
-        src={imgAvatarUrl}
-        alt="User avatar"
-        className="avatar"
-        width="300"
-      />
-      <p className="name">{props.username}</p>
-      <p className="tag">{tagAvatar}</p>
-      <p className="location">{locationAvatar}</p>
-    </div>
 
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">1000</span>
-      </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">2000</span>
-      </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">3000</span>
-      </li>
-    </ul>
-  </div>
-);
+// const Profile = props => (
+//   <div className="profile">
+//     <div className="description">
+//       <img
+//         src={imgAvatarUrl}
+//         alt="User avatar"
+//         className="avatar"
+//         width="300"
+//       />
+//       <p className="name">{nameAvatar}</p>
+//       <p className="tag">{tagAvatar}</p>
+//       <p className="location">{locationAvatar}</p>
+//     </div>
+
+//     <ul className="stats">
+//       <li>
+//         <span className="label">Followers</span>
+//         <span className="quantity">1000</span>
+//       </li>
+//       <li>
+//         <span className="label">Views</span>
+//         <span className="quantity">2000</span>
+//       </li>
+//       <li>
+//         <span className="label">Likes</span>
+//         <span className="quantity">3000</span>
+//       </li>
+//     </ul>
+//   </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Profile />);
+root.render(App);
